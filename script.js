a2e7d26596c44da61bad8d771844738c15f56254
mukeshrai1997 <135120496+mukeshrai1997@users.noreply.github.com>
2023-10-28 16:03:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-//your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function () {
+  // Get the form and input elements
+  const form = document.getElementById('preferences-form');
+  const fontSizeInput = document.getElementById('fontsize');
+  const fontColorInput = document.getElementById('fontcolor');
+
+  // Load and apply user preferences from cookies
+  if (localStorage.getItem('fontSize')) {
+    const savedFontSize = localStorage.getItem('fontSize');
+    document.body.style.fontSize = savedFontSize + 'px';
+    fontSizeInput.value = savedFontSize;
+  }
+
+  if (localStorage.getItem('fontColor')) {
+    const savedFontColor = localStorage.getItem('fontColor');
+    document.body.style.color = savedFontColor;
+    fontColorInput.value = savedFontColor;
+  }
+
+  // Handle form submission
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    // Get user preferences from form
+    const newFontSize = fontSizeInput.value;
+    const newFontColor = fontColorInput.value;
+
+    // Apply the new preferences to the page
+    document.body.style.fontSize = newFontSize + 'px';
+    document.body.style.color = newFontColor;
+
+    // Store preferences in cookies
+    localStorage.setItem('fontSize', newFontSize);
+    localStorage.setItem('fontColor', newFontColor);
+  });
+});
